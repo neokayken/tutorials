64a3ed06a2d1100b5a50da5500cb364be6afce0e
neokayken <99347133+neokayken@users.noreply.github.com>
2022-02-10 00:49:39 +0900

    Create integer_max_min.rs

---
+++ rust/integer_max_min.rs
@@ -0,0 +1,44 @@
+fn main() {
+
+    println!("\n[Rust Tutrorial]");
+
+    println!("--- Integer Types Signed ---\n");
+
+    println!("  i8 type Max : {:>20}", i8::MAX);
+    println!("  i8 type Min : {:>20}", i8::MIN);
+
+    println!(" i16 type Max : {:>20}", i16::MAX);
+    println!(" i16 type Min : {:>20}", i16::MIN);
+
+    println!(" i32 type Max : {:>20}", i32::MAX);
+    println!(" i32 type Min : {:>20}", i32::MIN);
+
+    println!(" i64 type Max : {:>20}", i64::MAX);
+    println!(" i64 type Min : {:>20}", i64::MIN);
+
+    println!("i128 type Max : {:>45}", i128::MAX);
+    println!("i128 type Min : {:>45}", i128::MIN);
+
+    println!("isize type Max : {:>45}", isize::MAX);
+    println!("isize type Min : {:>45}", isize::MIN);
+
+    println!("\n--- Integer Types Unsigned ---\n");
+
+    println!("  u8 type Max : {:>20}", u8::MAX);
+    println!("  u8 type Min : {:>20}", u8::MIN);
+
+    println!(" u16 type Max : {:>20}", u16::MAX);
+    println!(" u16 type Min : {:>20}", u16::MIN);
+
+    println!(" u32 type Max : {:>20}", u32::MAX);
+    println!(" u32 type Min : {:>20}", u32::MIN);
+
+    println!(" u64 type Max : {:>20}", u64::MAX);
+    println!(" u64 type Min : {:>20}", u64::MIN);
+
+    println!("u128 type Max : {:>45}", u128::MAX);
+    println!("u128 type Min : {:>45}", u128::MIN);
+
+    println!("usize type Max : {:>45}", usize::MAX);
+    println!("usize type Min : {:>45}", usize::MIN);
+}
